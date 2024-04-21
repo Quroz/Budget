9b8e438bd2c5f962115d86167f51ea92dac13133
Quroz <filipto@kth.se>
2024-04-21 15:30:59 +0200

    Init

---
+++ utils/Colors.jsx
@@ -0,0 +1,18 @@
+export default {
+    BLACK: "#000000",
+    PRIMARY: "#8B42FC",
+    WHITE: "#FFFFFF",
+    GRAY: "lightgray",
+    BLUE: "#4F75FE",
+    GREEN: "#13C38B",
+    PURPLE: "#9F3CFE",
+    RED: "#FF555D",
+    ORANGE: "#FF7D4F",
+    COLOR_LIST: [
+        "#4F75FE",
+        "#13C38B",
+        "#9F3CFE",
+        "#FF555D",
+        "#FF7D4F"
+    ]
+}
