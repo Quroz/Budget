1b0b3c2178b68abbcef6b6945c715eae81149d30
Quroz <filipto@kth.se>
2024-04-22 10:22:00 +0200

    ..

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { client } from '../utils/KindeConfig'
 import { supabase } from '../utils/SupabaseConfig'
+import { useRouter } from "expo-router"
 
 const Categories = () => {
 
     const [categories, setCategories] = useState([])
+    const router = useRouter()
 
     useEffect(() => {
         getCategory()
@@ -21,18 +23,25 @@ const Categories = () => {
     return (
         <View style={{ gap: 10 }}>
             {categories?.map((category, index) => (
-                <View style={{ padding: 10, backgroundColor: "white", borderRadius: 15, flexDirection: "row", alignItems: "center", justifyContent: "space-between" }} key={index}>
-                    <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
-                        <View style={{ backgroundColor: category?.color, height: 70, width: 70, borderRadius: 20, justifyContent: "center", alignItems: "center" }}>
-                            <Text style={{ fontSize: 35 }}>{category?.icon}</Text>
+                <TouchableOpacity onPress={() => router.push({
+                    pathname: "add-new-category-item",
+                    params: {
+                        categoryId: category.id,
+                    }
+                })}>
+                    <View style={{ padding: 10, backgroundColor: "white", borderRadius: 15, flexDirection: "row", alignItems: "center", justifyContent: "space-between" }} key={index}>
+                        <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+                            <View style={{ backgroundColor: category?.color, height: 70, width: 70, borderRadius: 20, justifyContent: "center", alignItems: "center" }}>
+                                <Text style={{ fontSize: 35 }}>{category?.icon}</Text>
+                            </View>
+                            <View>
+                                <Text style={{ fontFamily: "outfit-bold", fontSize: 20, }}>{category?.name}</Text>
+                                <Text style={{ fontFamily: "outfit-regular" }}>{category?.CategoryItems?.length} items</Text>
+                            </View>
                         </View>
-                        <View>
-                            <Text style={{ fontFamily: "outfit-bold", fontSize: 20, }}>{category?.name}</Text>
-                            <Text style={{ fontFamily: "outfit-regular" }}>{category?.CategoryItems?.length} items</Text>
-                        </View>
+                        <Text style={{ fontFamily: "outfit-bold", fontSize: 20 }}>$5000</Text>
                     </View>
-                    <Text style={{ fontFamily: "outfit-bold", fontSize: 20 }}>$5000</Text>
-                </View>
+                </TouchableOpacity>
             ))}
         </View>
 
